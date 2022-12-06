ee6b8d3a70a09d8d95a29851203244d22f349da0
ruby lingo <lingo.rubyyy@gmail.com>
2022-12-06 12:36:05 +0800

    added the functionality of printing even numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-
+        // Create a program that asks the user for a number and then prints the even numbers from 0 to the given number
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int counter = 0;
+        while (counter <= number) {
+            if (counter % 2 == 0) {
+                System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
